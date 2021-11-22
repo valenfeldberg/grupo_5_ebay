af462eadf9c25afed2660366d813a978563fa272
Julian Gabriel Valle <jvalle007@MBP-de-Julian.fibertel.com.ar>
2021-11-22 14:01:35 -0300

    agregando el register

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname + "/views/login.html"))
 })
 
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname + "/views/register.html"))
+})
+
 app.post('/', (req, res) => {
   res.sendFile(path.join(__dirname + "/views/index.html"))
 })
