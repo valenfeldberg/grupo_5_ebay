4c7f032a060b1e6a6287ef8a5eb6fe1db7eeaf99
javierbonaventura <bonaventura.javier@gmail.com>
2022-01-03 23:05:12 -0300

    Multer

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,129 +1,111 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-
-
-const productsFilePath = path.join(__dirname, './data/productos.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-
+const productsFilePath = path.join(__dirname, "./data/productos.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productController = {
+  getAll: (req, res) => {
+    const product = products;
+    res.render("index", { product: product });
+  },
 
-    getAll: (req, res ) => {
-      const product = products  
-      res.render("index", {product:product})
-      },
+  getAllMisPublicaciones: (req, res) => {
+    const product = products;
+    res.render("misPublicaciones", { product: product });
+  },
 
-      getAllMisPublicaciones: (req, res ) => {
-        const product = products  
-        res.render("misPublicaciones", {product:product})
-        },
+  login: (req, res) => {
+    const product = products;
+    res.render("login", {});
+  },
 
-    login: (req, res ) => {
-      const product = products  
-      res.render("login", {})
-      },
+  logged: (req, res) => {
+    const product = products;
+    res.render("logged", { product: product });
+  },
 
-    logged: (req, res ) => {
-      const product = products  
-      res.render("logged", {product:product})
-      },
+  carrito: (req, res) => {
+    const product = products;
+    res.render("carrito", { product: product });
+  },
 
-    carrito: (req, res ) => {
-      const product = products  
-      res.render("carrito", {product:product})
-      },
+  register: (req, res) => {
+    const product = products;
+    res.render("register", {});
+  },
 
-    register: (req, res ) => {
-      const product = products  
-      res.render("register", {})
-      },
+  create: (req, res) => {
+    res.render("productCreate", {});
+  },
 
-      create: (req, res) => {
-        res.render("productCreate", {})
-      },
+  getOne: (req, res) => {
+    const paramId = parseInt(req.params.id);
+    const product = products.find(
+      (productElement) => productElement.id === paramId
+    );
 
-      getOne: (req, res ) => {
-        const paramId = parseInt (req.params.id)
-        const product = products.find(productElement => productElement.id === paramId)
-      
-        if (product != null)
-        {
-          res.render("productDetail", {product})
-        }
-        else 
-        {
-      res.status(404).json({msg:"No esta el producto"})
-        }
-      },
-  
-        save: (req, res) => {
-         // const imagen = req.file.filename
-          const {nombre_producto, valor, ubicacion, usado } = req.body
-          const newProduct = {}
-          newProduct.id = products[products.length -1].id +1
-          newProduct.nombre_producto = nombre_producto
-          newProduct.valor = valor
-          newProduct.ubicacion = ubicacion
-          newProduct.usado = usado
+    if (product != null) {
+      res.render("productDetail", { product });
+    } else {
+      res.status(404).json({ msg: "No esta el producto" });
+    }
+  },
 
-          console.log(newProduct.id)
-          //newProduct.imagen = imagen
+  save: (req, res) => {
+    // const imagen = req.file.filename
+    const { nombre_producto, valor, ubicacion, usado } = req.body;
+    const newProduct = {};
+    newProduct.id = products[products.length - 1].id + 1;
+    newProduct.nombre_producto = nombre_producto;
+    newProduct.valor = valor;
+    newProduct.ubicacion = ubicacion;
+    newProduct.usado = usado;
+    newProduct.imagen = "/images/" + req.file.filename;
+    console.log(newProduct.id);
+    //newProduct.imagen = imagen
 
-          products.push(newProduct)
-          fs.writeFileSync(productsFilePath, JSON.stringify(products))
-          res.redirect("/product")
-        }, 
+    products.push(newProduct);
+    fs.writeFileSync(productsFilePath, JSON.stringify(products));
+    res.redirect("/product");
+  },
 
-        delete: (req, res) => {
-          const paramId = parseInt (req.params.id)
-          const productIndex = products.findIndex(productElement => productElement.id === paramId)
-          products.splice(productIndex, 1)
-          res.redirect(`/misPublicaciones`)
-          //fs.writeFileSync(productsFilePath, JSON.stringify(products))
-         }, 
+  delete: (req, res) => {
+    const paramId = parseInt(req.params.id);
+    const productIndex = products.findIndex(
+      (productElement) => productElement.id === paramId
+    );
+    products.splice(productIndex, 1);
+    res.redirect(`/misPublicaciones`);
+    //fs.writeFileSync(productsFilePath, JSON.stringify(products))
+  },
 
-         edit: (req, res) => {
-          
-          
-          const paramId = parseInt (req.params.id)
-          const product = products.find(productElement => productElement.id === paramId)
-        
-          if (product != null)
-          {
-            res.render("productEdit", {product})
-          }
-          else 
-          {
-        res.status(404).json({msg:"No esta el producto"})
-          }
-        
-        },
-          
+  edit: (req, res) => {
+    const paramId = parseInt(req.params.id);
+    const product = products.find(
+      (productElement) => productElement.id === paramId
+    );
 
+    if (product != null) {
+      res.render("productEdit", { product });
+    } else {
+      res.status(404).json({ msg: "No esta el producto" });
+    }
+  },
 
-        update: (req, res) => {
-          const productId = parseInt (req.params.id)
-          const product = products.find(productElement => productElement.id === productId)
-          product.nombre_producto =  req.body.nombre_producto
-          product.valor = req.body.valor
-          product.ubicacion = req.body.ubicacion
-          product.descripcion = req.body.descripcion
-          
-          //fs.writeFileSync(productsFilePath, JSON.stringify(products))
-          res.redirect("/product")
-          
+  update: (req, res) => {
+    const productId = parseInt(req.params.id);
+    const product = products.find(
+      (productElement) => productElement.id === productId
+    );
+    product.nombre_producto = req.body.nombre_producto;
+    product.valor = req.body.valor;
+    product.ubicacion = req.body.ubicacion;
+    product.descripcion = req.body.descripcion;
 
-        
-      },
-          
-      
-      
-
-    
-
-}
-
-
+    //fs.writeFileSync(productsFilePath, JSON.stringify(products))
+    res.redirect("/product");
+  },
+};
 
 module.exports = productController;
--- routes/product.js
+++ routes/product.js
@@ -1,28 +1,36 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const productController = require('../controllers/productController');
+const productController = require("../controllers/productController");
+const multer = require("multer");
+const path = require("path");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve("./public/images"));
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+const upload = multer({ storage });
 /* GET home page. */
 
-router.get('/', productController.getAll);
-router.get('/product', productController.getAll);
-router.get('/login', productController.login);
-router.get('/logged', productController.logged);
-router.get('/carrito', productController.carrito);
-router.get('/register', productController.register);
-router.get('/productCreate', productController.create);
-router.get('/product/:id', productController.getOne);
-router.post('/productCreate', productController.save);
-router.get('/misPublicaciones', productController.getAllMisPublicaciones);
-router.delete('/product/:id', productController.delete);
+router.get("/", productController.getAll);
+router.get("/product", productController.getAll);
+router.get("/login", productController.login);
+router.get("/logged", productController.logged);
+router.get("/carrito", productController.carrito);
+router.get("/register", productController.register);
+router.get("/productCreate", productController.create);
+router.get("/product/:id", productController.getOne);
+router.post("/productCreate", upload.single("imagen"), productController.save);
+router.get("/misPublicaciones", productController.getAllMisPublicaciones);
+router.delete("/product/:id", productController.delete);
 
 // Formulario de edición de productos (GET)
-router.get('/product/:id/productEdit', productController.edit);
+router.get("/product/:id/productEdit", productController.edit);
 
 // Acción de edición (a donde se envía el formulario) (PUT)
-router.put('/product/:id', productController.update);
-
-
-
+router.put("/product/:id", productController.update);
 
 module.exports = router;
